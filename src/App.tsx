f0e267c422df04241fd98da3fb507ed99197849a
Andrey Holod <andrejholod77@gmail.com>
2024-06-10 22:45:44 +0400

    Change the old method of adding routing to the new one

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { MouseEvent, useState } from 'react'
 import Button from './components/Button/Button'
 import Input from './components/Input/Input'
-import { Route, Routes } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Menu } from './pages/Menu'
 import { Cart } from './pages/Cart'
 import { ErrorPage } from './pages/ErrorPage'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: "*",
+    element: <ErrorPage />
+  }
+])
+
 function App() {
   const [counter, setCounter] = useState<number>(0)
-  console.log(counter)
 
   const addCounter = (e: MouseEvent) => {
     console.log(e.target)
@@ -18,16 +32,12 @@ function App() {
     <>
       <Button onClick={addCounter}>Кнопка</Button>
       <Button appearence='big' onClick={addCounter}>Кнопка</Button>
+      <Input placeholder='Email' />
       <div>
         <a href="/">Menu</a>
         <a href="/cart">Cart</a>
       </div>
-      <Input placeholder='Email'/>
-      <Routes>
-          <Route path='/' element={<Menu />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='*' element={<ErrorPage />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   )
 }
